5893823a6e882b3c209b0c177e057936b9530ff8
RetrebutionMk2 <87110744+RetrebutionMk2@users.noreply.github.com>
2024-04-29 17:53:07 -0400

    badwords.py

--- badwords.py
+++ badwords.py
@@ -1,17 +1,6 @@
 async def get_badwords_data():
     data = {
         "bad_words": [
-            "nigga",
-            "niggers",
-            "niggas",
-            "nigger",
-            "racist",
-            "rapist",
-            "rape",
-            "hitler",
-            "heil",
-            "hail",
-            "cum"
         ]
     }
     return data
